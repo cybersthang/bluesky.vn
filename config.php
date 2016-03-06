d4a3d76b837fe51209e951e531508112a1645f09
CybersThang <ducthangict.dhtn@gmail.com>
2016-03-06 21:46:55 +0700

    HTMl

--- config.php
+++ config.php
@@ -20,9 +20,9 @@ define('DIR_UPLOAD', 'D:/xampp/htdocs/bluesky.vn/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_HOSTNAME', '192.168.100.5');
+define('DB_USERNAME', 'cybersthang');
+define('DB_PASSWORD', 'admin');
 define('DB_DATABASE', 'bluesky.vn');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'bs_');
