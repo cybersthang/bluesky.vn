1bc910bda99888c5afdc71b6577057001d0ad771
luuvanhoa001 <luuvanhoa001@gmail.com>
2016-03-07 09:03:10 +0700

    UP

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bluesky.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bluesky.vn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/bluesky.vn/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/bluesky.vn/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/bluesky.vn/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/bluesky.vn/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/bluesky.vn/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/bluesky.vn/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/bluesky.vn/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/bluesky.vn/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/bluesky.vn/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/bluesky.vn/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/bluesky.vn/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '192.168.100.5');
+define('DB_USERNAME', 'cybersthang');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'bluesky.vn');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bs_');
