ad2ef850ce93e0a45e4d0a36f6b7e803076adf43
luuvanhoa <luuvanhoa93@gmail.com>
2016-03-10 20:48:44 +0700

    UP

--- catalog/language/english/common/footer.php
+++ catalog/language/english/common/footer.php
@@ -1,8 +1,21 @@
 <?php
 // Text
-$_['text_information']  = 'Information';
-$_['text_service']      = 'Customer Service';
-$_['text_extra']        = 'Extras';
+$_['text_information']  = 'Về chúng tôi';
+$_['text_service']      = 'Dịch vụ';
+$_['text_certification']= 'Chứng nhận';
+$_['text_address']      = '480/20 Lê Đức Thọ, p.16, Quận Gò Vấp, Tp.HCM';
+$_['text_hotline']      = '0123456789';
+$_['text_telephone']    = '0123456789';
+$_['text_email']        = 'thangvo@icsc.vn';
+$_['text_keyword']      = 'Google Addwords';
+$_['text_fb_maketing']  = 'Facebook Marketing';
+$_['text_yt_maketing']  = 'Youtube Marketing';
+$_['text_skype1']      = 'Luckystar.thangvo';
+$_['text_skype2']      = 'luuhoabmt';
+$_['text_skype3']      = 'kci3.vn';
+
+
+$_['text_extra']        = 'Hỗ trợ viên';
 $_['text_contact']      = 'Contact Us';
 $_['text_return']       = 'Returns';
 $_['text_sitemap']      = 'Site Map';
