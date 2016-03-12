6d5f145e62b7e38a71f47c70ef36d313e96e3e88
CybersThang <ducthangict.dhtn@gmail.com>
2016-03-12 19:42:28 +0700

    Commit 12/03/2016

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://localhost/bluesky.vn/');
 define('HTTPS_SERVER', 'http://localhost/bluesky.vn/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/bluesky.vn/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/bluesky.vn/system/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/bluesky.vn/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/bluesky.vn/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/bluesky.vn/system/config/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/bluesky.vn/image/');
-define('DIR_CACHE', 'C:/xampp/htdocs/bluesky.vn/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/bluesky.vn/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/bluesky.vn/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/bluesky.vn/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/bluesky.vn/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/xampp/htdocs/bluesky.vn/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/bluesky.vn/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/bluesky.vn/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/bluesky.vn/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/bluesky.vn/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/bluesky.vn/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/bluesky.vn/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/bluesky.vn/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/bluesky.vn/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/bluesky.vn/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/bluesky.vn/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
